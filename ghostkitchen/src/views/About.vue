4cc1901df7a5d6ead9eadd98d5180c5b8453ca43
Umang A <umanga.eng@gmail.com>
2021-07-09 17:50:29 -0400

    about page under dev

--- ghostkitchen/src/views/About.vue
+++ ghostkitchen/src/views/About.vue
@@ -1,21 +1,22 @@
 <template>
   <v-container>
-    <v-row>
-      <v-col cols="8">
-        <v-card flat>
-          <v-card-title> Contact Us</v-card-title>
-          <v-card-subtitle
-            >Please provide your information and we will contact you
-            soon!</v-card-subtitle
-          >
-          <v-card-text>
-            <v-form>
-              <v-input-field>Your name</v-input-field>
-            </v-form>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
+    <v-card flat>
+      <v-form>
+        <v-row>
+          <v-col cols="8">
+            <v-card-title> Contact Us</v-card-title>
+            <v-card-subtitle
+              >Please provide your information and we will contact you
+              soon!</v-card-subtitle
+            >
+            <v-card-text>
+              <!-- <v-text-field label="Your name" required></v-text-field> -->
+              <h1>Under development</h1>
+            </v-card-text>
+          </v-col>
+        </v-row>
+      </v-form>
+    </v-card>
   </v-container>
 </template>
 
